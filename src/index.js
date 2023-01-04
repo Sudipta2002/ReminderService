4e0ec39853f445ebe05c8bce95c8e9633514bccb
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2023-01-04 10:17:32 +0530

    nodemailer done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
-
+const { sendBasicEmail } = require('./services/email-service');
 const setUpAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.listen(PORT, () => {
         console.log(`Server is running at port ${PORT}`);
+        sendBasicEmail(
+            'support@admin.com',
+            'khelahobe.aj000@gmail.com',
+            'This is a testing email',
+            'Hey babes , I hope you like the reminder service support. Thanks !'
+        );
     })
 }
 setUpAndStartServer();
