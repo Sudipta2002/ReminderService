337a7493c6ba8c043ac48af15386df8ca0ce6259
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2023-01-04 10:33:05 +0530

    demo cron setup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
-const { sendBasicEmail } = require('./services/email-service');
+// const { sendBasicEmail } = require('./services/email-service');
+const cron = require('node-cron');
 const setUpAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.listen(PORT, () => {
         console.log(`Server is running at port ${PORT}`);
-        sendBasicEmail(
-            'support@admin.com',
-            'khelahobe.aj000@gmail.com',
-            'This is a testing email',
-            'Hey babes , I hope you like the reminder service support. Thanks !'
-        );
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'khelahobe.aj000@gmail.com',
+        //     'This is a testing email',
+        //     'Hey babes , I hope you like the reminder service support. Thanks !'
+        // );
+        cron.schedule('*/2 * * * *', () => {
+            console.log("Running a task every two minutes");
+        })
     })
 }
 setUpAndStartServer();
