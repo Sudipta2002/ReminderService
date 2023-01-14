ab922dc710eafbf1359eca68d461ce7a03acf137
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2023-01-14 22:59:50 +0530

    rabbit mq config

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
+const { createChannel } = require('./utils/messageQueue');
 // const { sendBasicEmail } = require('./services/email-service');
 const jobs = require('./utils/job');
 const TicketController = require('./controllers/ticket-controller');
-const setUpAndStartServer = () => {
+const setUpAndStartServer = async() => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    // const channel = await createChannel();
     app.post('/api/v1/tickets', TicketController.create);
     app.listen(PORT, () => {
         console.log(`Server is running at port ${PORT}`);
-        jobs();
-        // sendBasicEmail(
-        //     'support@admin.com',
-        //     'khelahobe.aj000@gmail.com',
-        //     'This is a testing email',
-        //     'Hey babes , I hope you like the reminder service support. Thanks !'
-        // );
-        // cron.schedule('*/2 * * * *', () => {
-        //     console.log("Running a task every two minutes");
-        // });
+        // jobs();
     });
 }
 
